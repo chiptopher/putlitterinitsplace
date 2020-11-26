b432325372cdadf79df6e9c2fb97aff65d0ea56d
chiptopher <chrisboyerdev@gmail.com>
2020-11-26 16:24:18 -0500

    Move assets/ to gfx/

--- litterfactory.lua
+++ litterfactory.lua
@@ -4,12 +4,12 @@ function createLitter()
     local rand = math.random(6)
     local litter = nil
     local position = math.random(50, 1300)
-    if     rand == 1 then litter = Litter:new(position, 0, 100, 100, love.graphics.newImage("assets/can.png"))
-    elseif rand == 2 then litter = Litter:new(position, 0, 100, 100, love.graphics.newImage("assets/bag.png"))
-    elseif rand == 3 then litter = Litter:new(position, 0, 100, 100, love.graphics.newImage("assets/banana.png"))
-    elseif rand == 4 then litter = Litter:new(position, 0, 100, 100, love.graphics.newImage("assets/bottle.png"))
-    elseif rand == 5 then litter = Litter:new(position, 0, 100, 100, love.graphics.newImage("assets/cigerette.png"))
-    elseif rand == 6 then litter = Litter:new(position, 0, 100, 100, love.graphics.newImage("assets/paper.png"))
+    if     rand == 1 then litter = Litter:new(position, 0, 100, 100, love.graphics.newImage("gfx/can.png"))
+    elseif rand == 2 then litter = Litter:new(position, 0, 100, 100, love.graphics.newImage("gfx/bag.png"))
+    elseif rand == 3 then litter = Litter:new(position, 0, 100, 100, love.graphics.newImage("gfx/banana.png"))
+    elseif rand == 4 then litter = Litter:new(position, 0, 100, 100, love.graphics.newImage("gfx/bottle.png"))
+    elseif rand == 5 then litter = Litter:new(position, 0, 100, 100, love.graphics.newImage("gfx/cigerette.png"))
+    elseif rand == 6 then litter = Litter:new(position, 0, 100, 100, love.graphics.newImage("gfx/paper.png"))
     end
     return litter
 end
