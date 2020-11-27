735a18947c31692e3b8ce783b33b1e99eccbe816
chiptopher <chrisboyerdev@gmail.com>
2020-11-27 07:58:08 -0500

    Add macOs flag to build script

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+rm -rf out/
 mkdir out/
 
 cp *.lua out/
@@ -7,9 +8,21 @@ pushd out
      zip -9 -r PutLitterInItsPlace.love .
 popd
 
-wget https://github.com/love2d/love/releases/download/11.3/love-11.3-macos.zip
-unzip love-11.3-macos.zip
 
-mv love.app PutLitterInItsPlace.app
-mv ./out/PutLitterInItsPlace.love PutLitterInItsPlace.app/Contents/Resources/
-yes | cp ./static/Info.plist PutLitterInItsPlace.app/Contents/
+function build_macos {
+    rm -rf *.app*
+    rm -rf *.zip*
+    wget https://github.com/love2d/love/releases/download/11.3/love-11.3-macos.zip
+    unzip love-11.3-macos.zip
+    mv love.app PutLitterInItsPlace.app
+    mv ./out/PutLitterInItsPlace.love PutLitterInItsPlace.app/Contents/Resources/
+    yes | cp ./static/Info.plist PutLitterInItsPlace.app/Contents/
+
+    zip -r PutLitterInItsPlace_macos.zip PutLitterInItsPlace.app
+
+}
+
+if [[ $* == *--macOs* ]]
+then
+    build_macos
+fi
