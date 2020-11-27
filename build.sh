ed3d563b35f357bbe1d81118b35abf43fea3bfff
chiptopher <chrisboyerdev@gmail.com>
2020-11-27 08:34:50 -0500

    Add windows build

--- build.sh
+++ build.sh
@@ -8,13 +8,13 @@ pushd out
      zip -9 -r PutLitterInItsPlace.love .
 popd
 
+rm -rf *.zip*
 
 function build_macos {
     rm -rf *.app*
-    rm -rf *.zip*
     wget https://github.com/love2d/love/releases/download/11.3/love-11.3-macos.zip
     unzip love-11.3-macos.zip
-    mv love.app PutLitterInItsPlace.app
+    cp love.app PutLitterInItsPlace.app
     mv ./out/PutLitterInItsPlace.love PutLitterInItsPlace.app/Contents/Resources/
     yes | cp ./static/Info.plist PutLitterInItsPlace.app/Contents/
 
@@ -22,7 +22,26 @@ function build_macos {
 
 }
 
+function build_windows {
+    rm *.exe*
+    rm -rf love-11.3-win64
+    wget https://github.com/love2d/love/releases/download/11.3/love-11.3-win64.zip
+    unzip love-11.3-win64.zip
+    mkdir PutLitterInItsPlace_win64
+    pushd love-11.3-win64
+        cat love.exe ../out/PutLitterInItsPlace.love > ../PutLitterInItsPlace_win64/PutLitterInItsPlace.exe
+        cp *.dll ../PutLitterInItsPlace_win64
+        cp ./license.txt ../PutLitterInItsPlace_win64/
+    popd
+    zip -r PutLitterInItsPlace_win64.zip PutLitterInItsPlace_win64
+}
+
 if [[ $* == *--macOs* ]]
 then
     build_macos
+fi
+
+if [[ $* == *--win* ]]
+then
+    build_windows
 fi
